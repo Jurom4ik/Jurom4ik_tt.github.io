c25692a3bfb538ccb96a11dd8cfc535b51f60dc6
Jurom4ik <pm929720@gmail.com>
2025-04-17 18:12:00 +0300

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,7 +1,1 @@
-let cartCount = 0;
 
-function addToCart(item, price) {
-  cartCount++;
-  document.getElementById('cart-count').innerText = cartCount;
-  alert(`Добавлено в корзину: ${item} (${price} грн)`);
-}
